47de19446a858564f696a32943290588a3dc21f5
I561796 I561796 <calin.tianu@sap.com>
2022-05-26 08:17:31 +0000

    Fix

--- ui/resources/webapp/core/utils/Formatters.js
+++ ui/resources/webapp/core/utils/Formatters.js
@@ -1,47 +1,23 @@
 sap.ui.define([
-	"sap/ui/core/format/NumberFormat"
-], function (NumberFormat) {
-	"use strict";
-	let oFormatOptions = {};
-	switch (sap.ui.getCore().getConfiguration().getLanguage()) {
-	case "en-US":
-		oFormatOptions = {
-			minIntegerDigits: 1,
-			maxIntegerDigits: 10,
-			minFractionDigits: 0,
-			maxFractionDigits: 2,
-			groupingEnabled: true,
-			groupingSeparator: ",",
-			decimalSeparator: "."
-		};
-		break;
-	case "de":
-		oFormatOptions = {
-			minIntegerDigits: 1,
-			maxIntegerDigits: 10,
-			minFractionDigits: 0,
-			maxFractionDigits: 2,
-			groupingEnabled: true,
-			groupingSeparator: ".",
-			decimalSeparator: ","
+		"sap/ui/core/format/NumberFormat"
+	], function (NumberFormat) {
+		"use strict";
+		let oFloatFormat = NumberFormat.getFloatInstance({
+				minIntegerDigits: 1,
+				maxIntegerDigits: 10,
+				minFractionDigits: 0,
+				maxFractionDigits: 2,
+				groupingEnabled: true
+			},
+			sap.ui.getCore().getConfiguration().getLocale());
 
+		return {
+			floatFormat: function (value) {
+
+				return oFloatFormat.format(value);
+
+			}
 		};
-		break;
 	}
-	return {
 
-		formatStringFloatValue: function (value) {
-
-			let oFloatFormat = NumberFormat.getFloatInstance(oFormatOptions);
-
-			return oFloatFormat.format(oFloatFormat.parse(value));
-		},
-
-		formatFloatValue: function (value) {
-
-			let oFloatFormat = NumberFormat.getFloatInstance(oFormatOptions);
-
-			return oFloatFormat.format(value);
-		}
-	};
-});
+);
